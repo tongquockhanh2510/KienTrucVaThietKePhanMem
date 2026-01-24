f05ffb521e12b9b9947bfe6e10bca8c05e7c1037
tongquockhanh2510 <22677951.khanh@student.iuh.edu.vn>
2026-01-24 16:10:14 +0700

    tuan02

---
+++ Tuan02/PerformanceTest/src/main/java/fit/performancetest/PerformanceTestApplication.java
@@ -0,0 +1,13 @@
+package fit.performancetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceTestApplication.class, args);
+    }
+
+}
---
+++ Tuan02/PerformanceTest/src/test/java/fit/performancetest/PerformanceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.performancetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
